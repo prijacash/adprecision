d37876e129d05b209f1a7e45938f9189d52e39df
prijacash <prijacash@gmail.com>
2022-11-10 12:44:33 -0800

    nav and app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+// pages
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Certificate from './components/pages/Certificate'
+import Engineering from './components/pages/Engineering'
+
+
+// partials
+import Navbar from './components/partials/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header>
+          <NavBar />
+        </header>
+
+        <div>
+            <Route 
+              path='/'
+              element={<Home />}          
+            />
+
+            <Route 
+              path='/about'
+              element={<About />}          
+            />
+
+            <Route 
+              path='/engineering'
+              element={<Engineering />}          
+            />
+
+            <Route 
+              path='/certificate'
+              element={<Certificate />}          
+            />
+        </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
