b83da0b0531c127f0495bb3a9490a127d2b48153
devin-lynch <devin.l.lynch@gmail.com>
2022-11-10 21:23:18 -0600

    Add nav links

--- src/components/partials/NavBar.jsx
+++ src/components/partials/NavBar.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+
 export default function NavBar() {
     return (
         <div>
-            NavBar
+            <ul className='flex flex-row items-center justify-center gap-2'>
+                <li className='my-2'><Link to='/'>Home</Link></li>{' | '}
+                <li className=''><Link to='/about'>About</Link></li>{' | '}
+                <li className=''><Link to='/engineering'>Engineering</Link></li>{' | '}
+                <li className=''><Link to='/certificate'>Certificates</Link></li>
+            </ul>
         </div>
     )
 }
