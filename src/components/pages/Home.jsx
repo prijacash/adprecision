510a8c1a9acf5c9b887173c20ae5f897738d0254
devin-lynch <devin.l.lynch@gmail.com>
2022-11-15 21:56:00 -0600

    Import partials to home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,28 @@
+import GenOverview from '../partials/GenOverview'
+import Services from '../partials/Services'
+import Facility from '../partials/Facility'
+import SalesCustProfile from '../partials/SalesCustProfile'
+
 export default function Home() {
     return (
         <div>
-            Home Page
+            <p>Home Page</p>
+
+            <div className='mt-6'>
+                <GenOverview />
+            </div>
+
+            <div className='mt-6'>
+                <Services />
+            </div>
+
+            <div className='mt-6'>
+                <SalesCustProfile />
+            </div>
+
+            <div className='mt-6'>
+                <Facility />
+            </div>
         </div>
     )
 }
