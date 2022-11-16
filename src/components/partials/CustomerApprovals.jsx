1095400a2fffaf344c093d07210b1ddc40fe0463
devin-lynch <devin.l.lynch@gmail.com>
2022-11-15 21:46:29 -0600

    Add cust app and gen overview

--- src/components/partials/CustomerApprovals.jsx
+++ src/components/partials/CustomerApprovals.jsx
@@ -2,6 +2,8 @@ export default function CustomerApprovals() {
     return (
         <div>
             <p>CustomerApprovals</p>
+
+            <img src='https://images-ext-1.discordapp.net/external/W7KW7ONzNNSpJoqt8M_nmMlE5-BVyzSHIRPBD3g69rY/https/i.imgur.com/I6bL1nn.png' />
         </div>
     )
 }
---
+++ src/components/partials/GenOverview.jsx
@@ -0,0 +1,15 @@
+export default function GenOverview() {
+    return (
+        <div>
+            <p>General Overview</p>
+
+            <p>Multi-axis Machining</p>
+            <p>High Speed Machining up to 12,000 RPM</p>
+            <p>Machining of Hard Metal, Titanium, Inconel and P/H Harden Stainless Steel and High Nickel-Based Alloys</p>
+            <p>Turn-Key Part Development</p>
+            <p>Focus on On-Time Delivery</p>
+            <p>Rapid Prototyping</p>
+            <p>Approved Supplier for "Fracture Critical Part" Manufacturing for Northrop Grumman and Lockheed Martin</p>
+        </div>
+    )
+}
