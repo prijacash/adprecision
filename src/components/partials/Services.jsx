1f614432fe0f1a0a70f0e9f507a7f8dc8ee4c3b5
devin-lynch <devin.l.lynch@gmail.com>
2022-11-15 21:27:16 -0600

    Add services

--- src/components/partials/Services.jsx
+++ src/components/partials/Services.jsx
@@ -2,6 +2,16 @@ export default function Services() {
     return (
         <div>
             <p>Services</p>
+
+            <p>Catia V5 - CAD / CAM</p>
+            <p>In-house Tool Design and Fabrication</p>
+            <p>Advance Machining Techniques</p>
+            <p>Fast Turnaround on High Priority Parts</p>
+            <p>Full Material Traceability</p>
+            <p>ISO 9001:2015 and AS9100D Certified</p>
+            <p>Source Inspection Facilitation</p>
+            <p>Min/Max Inventory</p>
+            <p>Program Management</p>
         </div>
     )
 }
