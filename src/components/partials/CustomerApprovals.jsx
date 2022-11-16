a3a7dcdb3990d0e7be72d1531177231461d51ff3
prijacash <prijacash@gmail.com>
2022-11-15 19:17:28 -0800

    added components

---
+++ src/components/partials/CustomerApprovals.jsx
@@ -0,0 +1,7 @@
+export default function CustomerApprovals() {
+    return (
+        <div>
+            <p>CustomerApprovals</p>
+        </div>
+    )
+}
---
+++ src/components/partials/CustomerRecognition.jsx
@@ -0,0 +1,7 @@
+export default function CustomerRecognition() {
+    return (
+        <div>
+            <p>Customer Recognition</p>
+        </div>
+    )
+}
