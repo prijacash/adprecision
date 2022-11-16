f1a910b118dd84b7aed722e47f6b8a03ed3ffa2d
devin-lynch <devin.l.lynch@gmail.com>
2022-11-15 21:23:04 -0600

    Stub sales/cust profile

--- src/components/partials/SalesCustProfile.jsx
+++ src/components/partials/SalesCustProfile.jsx
@@ -2,6 +2,10 @@ export default function SalesCustProfile() {
     return (
         <div>
             <p>Sales and Customer Profile</p>
+
+            <p>Aerospace & Defense Primes</p>
+            <p>1st and 2nd Tier Aerospace</p>
+            <p>References Available Upon Request</p>
         </div>
     )
 }
