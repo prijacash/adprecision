af5a8cb7bab354a5e3efe34047383ec6fa223702
prijacash <prijacash@gmail.com>
2022-11-17 14:48:25 -0800

    engineering info

---
+++ src/components/partials/EngineeringInfo.jsx
@@ -0,0 +1,21 @@
+export default function EngineeringInfo() {
+    return (
+        <div>
+            <p>Engineering Information</p>
+
+            <div className="text-center">
+                <div className="text-left inline-block">
+                    <p>Multi-axis Machining</p>
+
+                    <p>A&D Precision Manufacturing utilizes the most to date CAD/CAM</p> <p>software to support tool design, engineering change activity,</p> <p>process planning , and programming for CNC Machines. </p>
+
+                    <p>CATIA V5</p>
+                    <p>SIEMENS NX (UG)</p>
+                    <p>GIBBSCAM</p>
+                    <p>VERISURF</p>
+                    <p>KUBOTEK</p>
+                </div>
+            </div>
+        </div>
+    )
+}
