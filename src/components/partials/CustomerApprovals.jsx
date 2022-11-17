81b9d8174abb1c88462a5ea71fbeb5e3a8a83d8c
devin-lynch <devin.l.lynch@gmail.com>
2022-11-17 15:38:33 -0600

    Test cust approvals

--- src/components/partials/CustomerApprovals.jsx
+++ src/components/partials/CustomerApprovals.jsx
@@ -1,9 +1,11 @@
 export default function CustomerApprovals() {
     return (
-        <div>
+        <div className="mt-6 justify-center">
             <p>CustomerApprovals</p>
 
-            <img src='https://images-ext-1.discordapp.net/external/W7KW7ONzNNSpJoqt8M_nmMlE5-BVyzSHIRPBD3g69rY/https/i.imgur.com/I6bL1nn.png' />
+            <div className='flex justify-center'>
+                <img src='https://images-ext-1.discordapp.net/external/W7KW7ONzNNSpJoqt8M_nmMlE5-BVyzSHIRPBD3g69rY/https/i.imgur.com/I6bL1nn.png' className='customerApprovals' />
+            </div>
         </div>
     )
 }
