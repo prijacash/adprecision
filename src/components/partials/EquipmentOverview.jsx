e424c9dbbbc749b1daef8d6a4f6285871c141283
prijacash <prijacash@gmail.com>
2022-11-17 15:26:25 -0800

    equipment

---
+++ src/components/partials/EquipmentOverview.jsx
@@ -0,0 +1,135 @@
+export default function CustomerRecognition() {
+    return (
+        <div className="mt-6 justify-center">
+            <p>A&D Precision Manufacturing, Inc. - Equipment Capabilities Quick Overview</p>
+            <table class="table-auto">
+
+            <thead>
+                <tr>
+                    <th>Machine</th>
+                    <th># Axis</th>
+                    <th>Travel X, Y, Z</th>
+                    <th>Travel A</th>
+                    <th>Travel C</th>
+                    <th>RPM Max</th>
+                    <th>Quantity</th>
+                    <th>Machine Type</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+                <tr>
+                    <td>Fadal  VMC-4020</td>
+                    <td>3</td>
+                    <td>40”X 20”X 20”</td>
+                    <td>N/A</td>
+                    <td>N/A</td>
+                    <td>10,000 RPM</td>
+                    <td>4</td>
+                    <td>Mill-High Speed Machine</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    )
+}
