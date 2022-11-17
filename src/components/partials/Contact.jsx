507f93e3cb2d7eb822f23fc6371136df93ad5fb9
prijacash <prijacash@gmail.com>
2022-11-17 13:45:54 -0800

    updated contact

---
+++ src/components/partials/Contact.jsx
@@ -0,0 +1,23 @@
+export default function Contact() {
+    return (
+        <div>
+            <p>Contact</p>
+
+            <div className="text-center">
+                <div className="text-left inline-block">
+                    <p> Sales and Customer Profile </p>
+                    <p>- Aerospace & Defense Primes </p>
+                    <p>- 1st and 2nd Tier Aerospace </p>
+                    <p>- References Available Upon Request</p>
+                </div>
+
+                <div className="text-left inline-block">
+                    <p> Sales Contact </p>
+                    <p>- Phone: 714-779-2714 </p>
+                    <p>- Email: pvo@adprecisionmfg.com </p>
+                    <p>- Location: 4751 E. Hunter, Anaheim, CA</p>
+                </div>
+            </div>
+        </div>
+    )
+}
