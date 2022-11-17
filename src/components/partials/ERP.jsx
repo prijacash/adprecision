2381d90b4e1dfd6ce155bbad3fb7a4230b184c5b
prijacash <prijacash@gmail.com>
2022-11-17 14:02:43 -0800

    initial setup ERP

---
+++ src/components/partials/ERP.jsx
@@ -0,0 +1,11 @@
+export default function ERP() {
+    return (
+        <div className="mt-6 justify-center">
+            <p>ERP System</p>
+
+            <div className='flex justify-center'>
+                <img src='https://i.imgur.com/9IGKPgr.png' className='organization' />
+            </div>
+        </div>
+    )
+}
