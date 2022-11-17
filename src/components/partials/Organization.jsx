aad121a898cd9f4cdd8aa3d6eeaa9079a1470652
prijacash <prijacash@gmail.com>
2022-11-17 13:57:45 -0800

    updated org chart

---
+++ src/components/partials/Organization.jsx
@@ -0,0 +1,11 @@
+export default function Organization() {
+    return (
+        <div className="mt-6 justify-center">
+            <p>Organization</p>
+
+            <div className='flex justify-center'>
+                <img src='https://i.imgur.com/g6ZFa1A.png' className='organization' />
+            </div>
+        </div>
+    )
+}
