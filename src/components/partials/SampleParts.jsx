c25416fdd7d555ee8f163f2e3ad18b0744a3af0a
prijacash <prijacash@gmail.com>
2022-11-18 12:54:43 -0800

    updated sampple

--- src/components/partials/SampleParts.jsx
+++ src/components/partials/SampleParts.jsx
@@ -4,7 +4,11 @@ export default function SampleParts() {
             <p>Sample Parts</p>
 
             <div className='flex justify-center'>
-                <img src='https://i.imgur.com/4veJElA.png' className='organization' />
+                <img src='https://i.imgur.com/4veJElA.png' className='sampleParts' />
+            </div>
+
+            <div className='flex justify-center'>
+                <img src='https://i.imgur.com/qvembhp.png' className='sampleParts' />
             </div>
         </div>
     )
