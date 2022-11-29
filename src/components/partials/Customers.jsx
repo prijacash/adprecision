f8b2511876c8947da4164c319a8fa8f528ce966a
prijacash <prijacash@gmail.com>
2022-11-29 15:22:21 -0800

    updated customers

--- src/components/partials/Customers.jsx
+++ src/components/partials/Customers.jsx
@@ -1,107 +1,86 @@
 export default function CustomerApprovals() {
     return (
+
+        
         <div className="mt-6 mb-8 justify-center">
-            <p className='text-xl italic font-semibold underline'>CustomerApprovals</p>
+            <p className='text-2xl font-semibold underline'>CustomerApprovals</p>
 
-            <div className='mt-2 flex justify-center'>
-                <img src='https://images-ext-1.discordapp.net/external/W7KW7ONzNNSpJoqt8M_nmMlE5-BVyzSHIRPBD3g69rY/https/i.imgur.com/I6bL1nn.png' className='customerApprovals' />
-            </div>
-
-            <p className='text-l italic font-semibold underline'>Sample Parts</p>
-
-            <section className="overflow-hidden text-gray-700 ">
-                <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-                    <div className="flex flex-wrap -m-1 md:-m-2">
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="northrop" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/tMJ14vS.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img alt="lockheed" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/HxYw83S.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="spacex" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/wQX2p4v.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="terma" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/ofDe6oM.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img alt="hamilton sun" 
-                                className="block object-cover object-center w-full h-full rounded-lg"
-                                src="https://i.imgur.com/sJptV3k.png"/>
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="gallery" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/tD3jtHV.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="gallery" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/CO8wwkN.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="gallery" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/Mh3xNyw.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="gallery" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/LEsSimr.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/5">
-                            <div className="w-full p-1 md:p-2">
-                                <img 
-                                    alt="gallery" 
-                                    className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://i.imgur.com/2YMbC8N.png"
-                                />
-                            </div>
-                        </div>
+           
+            <div className='mt-6 flex justify-center'>
+                <div className="w-3/5 grid grid-cols-5 gap-4">
+                    <div>
+                        <img 
+                            alt="northrop" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/tMJ14vS.png"
+                        />
+                    </div>
+                    <div>
+                        <img alt="lockheed" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/HxYw83S.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="spacex" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/wQX2p4v.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="terma" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/ofDe6oM.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="hamilton sun" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/sJptV3k.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="coherent" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/tD3jtHV.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="meggit" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/CO8wwkN.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="orbital atk" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/Mh3xNyw.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="parker" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/LEsSimr.png"
+                        />
+                    </div>
+                    <div>
+                        <img 
+                            alt="triumph group" 
+                            className="block object-cover object-center w-full rounded-lg"
+                            src="https://i.imgur.com/2YMbC8N.png"
+                        />
                     </div>
                 </div>
-            </section>
+
+
+            </div>
         </div>
     )
 }
