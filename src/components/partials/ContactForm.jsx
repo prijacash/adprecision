22980e1badb28757586f1955c486397fedb917db
prijacash <prijacash@gmail.com>
2022-12-05 12:13:50 -0800

    updated contact form and partial

--- src/components/partials/ContactForm.jsx
+++ src/components/partials/ContactForm.jsx
@@ -22,48 +22,53 @@ export default function ContactForm() {
     }
 
     return(
-
-        <form
-        action={FORM_ENDPOINT}
-        onSubmit={handleSubmit}
-        method="POST"
-        target="_blank"
-      >
-        <div className="mb-3 pt-0">
-          <input
-            type="text"
-            placeholder="Your name"
-            name="name"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <textarea
-            placeholder="Your message"
-            name="message"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <button
-            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="submit"
-          >
-            Send a message
-          </button>
-        </div>
-      </form>
+        <div className="mt-6">
+            <div className="mt-2 flex justify-center text-center">                   
+                <form
+                  action={FORM_ENDPOINT}
+                  onSubmit={handleSubmit}
+                  method="POST"
+                  target="_blank"
+                  className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                >
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Contact Us</h5>
+                  <div className="mb-3 pt-0">
+                    <input
+                      type="text"
+                      placeholder="Your name"
+                      name="name"
+                      className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                      required
+                    />
+                  </div>
+                  <div className="mb-3 pt-0">
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                      required
+                    />
+                  </div>
+                  <div className="mb-3 pt-0">
+                    <textarea
+                      placeholder="Your message"
+                      name="message"
+                      className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+                      required
+                    />
+                  </div>
+                  <div className="mb-3 pt-0">
+                    <button
+                      className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="submit"
+                    >
+                      Send a message
+                    </button>
+                  </div>
+                </form>
+            </div>
+      </div>
 
     )
 }
--- src/components/partials/ContactPartial.jsx
+++ src/components/partials/ContactPartial.jsx
@@ -1,22 +1,27 @@
 export default function ContactPartial() {
     return (
         <div>
-            <p>Contact</p>
+            <div className="mt-2 flex justify-center text-center">
+                <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Contact</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400"> 
+                    <div className="text-center justify-center flex space-x-20 mt-6">
+                        <div className="text-left inline-block">
+                            <p>Sales and Customer Profile </p>
+                            <p>Aerospace & Defense Primes </p>
+                            <p>1st and 2nd Tier Aerospace </p>
+                            <p>References Available Upon Request</p>
+                        </div>
 
-            <div className="text-center">
-                <div className="text-left inline-block">
-                    <p> Sales and Customer Profile </p>
-                    <p>- Aerospace & Defense Primes </p>
-                    <p>- 1st and 2nd Tier Aerospace </p>
-                    <p>- References Available Upon Request</p>
-                </div>
-
-                <div className="text-left inline-block">
-                    <p> Sales Contact </p>
-                    <p>- Phone: 714-779-2714 </p>
-                    <p>- Email: pvo@adprecisionmfg.com </p>
-                    <p>- Location: 4751 E. Hunter, Anaheim, CA</p>
-                </div>
+                        <div className="text-left inline-block">
+                            <p>Sales Contact </p>
+                            <p>714-779-2714 </p>
+                            <p>pvo@adprecisionmfg.com </p>
+                            <p>4751 E. Hunter, Anaheim, California</p>
+                        </div>
+                    </div>              
+                    </p>
+                </a>
             </div>
         </div>
     )
