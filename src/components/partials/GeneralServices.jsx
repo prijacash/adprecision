8f283d892ff8b8bdbc9184b4f6e61bd0d17ca405
devin-lynch <devin.l.lynch@gmail.com>
2022-12-05 13:39:32 -0600

    Add info to quality, materials, and gen serv

--- src/components/partials/GeneralServices.jsx
+++ src/components/partials/GeneralServices.jsx
@@ -1,7 +1,16 @@
 export default function GeneralServices() {
     return (
         <div>
-            <p>General Services</p>
+            <p>Our value position can help you strategically address your manufacturing needs. As an ITAR compliant, ISO9001/AS9100 certified business, A&D Precision Manufacturing specializes in delivering CNC precision machining, fabrication and assembly work to the Aeronautics, Military & Defense, Commercial Aviation, and various other industries. We are equipped to handle your projects from initial concept to full-scale manufacturing and assembly. We're passionate about turning your most complex ideas into reality and watching them revolutionize the industries you sell into. We work in parallel with you as your one-partner solution.</p>
+
+            <ul>
+                <li>CNC Milling (3, 4 and 5 axis)</li>
+                <li>CNC Turning</li>
+                <li>Component Assembly</li>
+                <li>Special Fabrication</li>
+                <li>Materials</li>
+                <li>Engineering & Design</li>
+            </ul>
         </div>
     )
 }
--- src/components/partials/Materials.jsx
+++ src/components/partials/Materials.jsx
@@ -1,7 +1,9 @@
 export default function Materials() {
     return (
         <div>
-            <p>Materials</p>
+            <p>A&D Precision Machining works with a wide range of materials to meet the continued advancements of product design, form, and function. Our depth of material knowledge and quality inspection provides our customers with assurance whether we source directly from their supplier or ours.</p>
+
+            {/* insert table here */}
         </div>
     )
 }
