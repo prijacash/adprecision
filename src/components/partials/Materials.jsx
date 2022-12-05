6ad40d2d8b5278a4df4a1f41ed19d21b3f43156c
prijacash <prijacash@gmail.com>
2022-12-05 12:48:22 -0800

    updated materials

--- src/components/partials/Materials.jsx
+++ src/components/partials/Materials.jsx
@@ -1,11 +1,19 @@
 export default function Materials() {
     return (
         <div>
-            <p>A&D Precision Machining works with a wide range of materials to meet the continued advancements of product design, form, and function. Our depth of material knowledge and quality inspection provides our customers with assurance whether we source directly from their supplier or ours.</p>
 
-            {/* insert table here */}
-            <div className='flex justify-center text-center mt-6'>
+            <div className="mt-2 flex justify-center text-center">
                 <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Materials</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400"> 
+                        <ul>
+                            <li className='mb-2'>
+                                A&D Precision Machining works with a wide range of materials to meet the continued advancements of product design, form, and function. Our depth of material knowledge and quality inspection provides our customers with assurance whether we source directly from their supplier or ours.
+                            </li>
+                        </ul>                
+                    </p>
+                    
+                    {/* insert table here */}
                     <div className='flex justify-center items-center'>
                         <table class='table-auto border-separate [border-spacing:1rem]'>
                             <thead className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -42,7 +50,7 @@ export default function Materials() {
                                 </tr>
                             </tbody>
                         </table>
-                    </div>
+                    </div>                    
                 </a>
             </div>
         </div>
