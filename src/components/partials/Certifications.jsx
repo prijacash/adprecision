f1e82404d3091c7568d16e0bdaf4f29231564ee0
prijacash <prijacash@gmail.com>
2022-12-05 11:54:00 -0800

    updated quality

--- src/components/partials/Certifications.jsx
+++ src/components/partials/Certifications.jsx
@@ -1,27 +1,21 @@
 export default function Certifications() {
     return (
         <div className='flex justify-center mb-10'>
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-
-                <div className='flex justify-center mt-2'>
-                    <img className="rounded-t-lg justify-center align-center" src="https://i.imgur.com/JFSu1lJ.png" alt="" />
-                </div>
-
-                
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            Registration and Certification</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <div className="mt-2 flex justify-center text-center">
+                <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Registration and Certification</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400 inline-block"> 
+                    <ul>
+                        <li className='mb-2'>
                             ISO 9001:2015 and AS9100D <br></br>
                             ITAR Registration Code: M28003
+                        </li>
+                        <li className='mb-2 inline-block'>
+                            <img className="rounded-t-m" src="https://i.imgur.com/gIkPaE1.png" alt=""/><img className="rounded-t-m" src="https://i.imgur.com/rNcZB8b.png" alt=""/>
+                        </li>
+                    </ul>                
                     </p>
-
-                    <img className="rounded-t-lg " src="https://i.imgur.com/gIkPaE1.png" alt=""/>
-                    <img className="rounded-t-lg " src="https://i.imgur.com/rNcZB8b.png" alt=""/>
-                    
-                </div>
+                </a>
             </div>
         </div>
     )
--- src/components/partials/QualityOverview.jsx
+++ src/components/partials/QualityOverview.jsx
@@ -1,9 +1,21 @@
 export default function QualityOverview() {
     return (
         <div>
-            <p>A&D Precision Manufacturing is firmly dedicated to providing excellence in quality.  In addition to the industry standard certifications and quality systems, our metrology lab is designed to enhance measurement accuracy, reduce uncertainty and deliver the fully inspected product on time and on budget.  Our company is set-up to handle in-process checks as well as final inspection for the full spectrum of requirements.</p>
-
-            <p>At A&D Precision Manufacturing, we understand that reliability of our process rests upon the accuracy of our equipment and knowledge of our technicians.</p>
+            <div className="mt-2 flex justify-center text-center">
+                <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Quality Overview</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400"> 
+                    <ul>
+                        <li className='mb-2'>
+                            A&D Precision Manufacturing is firmly dedicated to providing excellence in quality.  In addition to the industry standard certifications and quality systems, our metrology lab is designed to enhance measurement accuracy, reduce uncertainty and deliver the fully inspected product on time and on budget.  Our company is set-up to handle in-process checks as well as final inspection for the full spectrum of requirements.
+                        </li>
+                        <li className='mb-2'>
+                            At A&D Precision Manufacturing, we understand that reliability of our process rests upon the accuracy of our equipment and knowledge of our technicians.
+                        </li>
+                    </ul>                
+                    </p>
+                </a>
+            </div>
         </div>
     )
 }
