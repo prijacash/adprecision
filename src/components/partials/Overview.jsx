6a9ab15af48dae9d222c544453031ce1e2b68fc8
devin-lynch <devin.l.lynch@gmail.com>
2022-12-13 20:14:48 -0600

    Debug font for theme on overview

--- src/components/partials/Overview.jsx
+++ src/components/partials/Overview.jsx
@@ -4,7 +4,7 @@ export default function Overview() {
             <div className="mt-2 flex justify-center text-center">
                 <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 opacity-70">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About A&D Precision</h5>
-                    <p className="font-normal text-gray-700 dark:text-gray-400"> 
+                    <p className="font-normal text-gray-900 dark:text-white"> 
                     <ul>
                         <li className='mb-2'>
                             A&D Precision Manufacturing, Inc was founded in 1976 and headquartered in Anaheim, California, is a leading manufacturer of small and medium, complex structural components and sub-assemblies for the commercial and military aircraft markets.
