6f8c88a9319d806e8b12ea9c8273b780cd6ec667
devin-lynch <devin.l.lynch@gmail.com>
2022-12-13 20:59:15 -0600

    Remove mt all components

--- src/components/partials/GeneralServices.jsx
+++ src/components/partials/GeneralServices.jsx
@@ -1,7 +1,7 @@
 export default function GeneralServices() {
     return (
         <div>
-            <div className="mt-2 flex justify-center text-center">
+            <div className=" flex justify-center text-center">
                 <a href="#" className="block w-3/5 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 opacity-70">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">General Services</h5>
                     <p className="font-normal text-gray-900 dark:text-white"> 
