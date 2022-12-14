91a99891e005c5189d9b176cd5402030b22a8462
prijacash <prijacash@gmail.com>
2022-12-13 18:03:21 -0800

    updated hero

--- src/components/partials/Hero.jsx
+++ src/components/partials/Hero.jsx
@@ -1,13 +1,13 @@
 export default function Hero() {
     return (
         <div>
-            <section class="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
-                <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
-                    <video class="min-w-full min-h-full absolute object-cover" src="https://josecuono.dev/video1.mp4" type="video/mp4" autoplay muted loop></video>
+            <section className="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
+                <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+                    <video className="min-w-full min-h-full absolute object-cover" src="https://josecuono.dev/video1.mp4" type="video/mp4" autoplay muted loop></video>
                 </div>
-                <div class="video-content space-y-2">
-                    <h1 class="font-light text-6xl">full Hero Video</h1>
-                    <h3 class="font-light text-3xl">with TailwindCSS</h3>
+                <div className="video-content space-y-2">
+                    <h1 className="font-light text-6xl">full Hero Video</h1>
+                    <h3 className="font-light text-3xl">with TailwindCSS</h3>
                 </div>
             </section>
         </div>
