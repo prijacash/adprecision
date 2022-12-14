9059037641e757973e4845001c2d39653b69ef6b
prijacash <prijacash@gmail.com>
2022-12-13 18:42:11 -0800

    updated contact

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,9 @@ import ContactPartial from '../partials/ContactPartial'
 
 export default function Contact() {
     return (
-        <div>
+        <div className='mb-12 bg-fixed' style={{backgroundImage: `url(https://i.imgur.com/Oo409lC.png)`, backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat'}}>
+
             {/* <p>Contact</p> */}
 
             <ContactPartial />
